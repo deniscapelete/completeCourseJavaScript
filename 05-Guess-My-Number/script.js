35a595f0f8e311e7f9329277109114aaf8cee57e
Denis <capelete.denis@hotmail.com>
2024-04-04 20:48:15 -0300

    Part 5 - Tratamento de eventos de clique

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+
+document.querySelector('.message').textContent = 'correct Number';
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 12;
+
+console.log(document.querySelector('.guess').value);
+
+document.querySelector('.guess').value = 23;
+
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!'
+    }
+});
