297dc1e174b0d661094f7a73f6e6fa32726324d7
Denis <capelete.denis@hotmail.com>
2024-04-11 16:50:55 -0300

    Part 6 - PROJECT #2: Modal Window - Lidando com um evento de pressionamento de tecla 'Esc'

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -23,4 +23,11 @@ btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+    console.log(e.key)
+    if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
+
 
