1122ffb22c2d54eec5180a45184ca62673e968a6
Denis <capelete.denis@hotmail.com>
2024-04-13 18:09:55 -0300

    Starting Part 7 - PROJECT #3: Pig Game, iniciando a funcionalidade do botão HOLD

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,10 @@ let dice;
 let current = function (current, currentPlayer) {
     document.querySelector('#current--' + currentPlayer).textContent = current;
 }
+let playerActive = function (playerNumber) {
+    return document.querySelector('.player--' + playerNumber).classList;
+}
+
 let playerNumber = 0;
 
 document.querySelector('.btn--roll').addEventListener('click', function () {
@@ -19,6 +23,23 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
         current(0, playerNumber);
     }
 
+});
+
+document.querySelector('.btn--hold').addEventListener('click', function () {
+
+    document.querySelector('#score--' + playerNumber).textContent = document.querySelector('#current--' + playerNumber).textContent;
+    current(0, playerNumber);
+    playerActive(playerNumber).remove('player--active');
+
+
+    if (playerNumber == 1) {
+        playerNumber--;
+        playerActive(playerNumber).add('player--active');
+    } else {
+        playerNumber++;
+        playerActive(playerNumber).add('player--active');
+
+    }
 });
 
 
