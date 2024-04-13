24887cc56e49bf71897e9bac345e616f7353987e
Denis <capelete.denis@hotmail.com>
2024-04-13 17:19:36 -0300

    Starting Part 7 - PROJECT #3: Pig Game, iniciando a funcionalidade do botão ROLL DICE

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,24 @@
 'use strict';
+let dice;
+let current = function (current, currentPlayer) {
+    document.querySelector('#current--' + currentPlayer).textContent = current;
+}
+let playerNumber = 0;
+
+document.querySelector('.btn--roll').addEventListener('click', function () {
+
+    dice = Math.trunc(Math.random() * 6) + 1;
+
+    document.querySelector('.dice').setAttribute('src', 'dice-' + dice + '.png');
+
+
+    current(Number(document.querySelector('#current--' + playerNumber).textContent) + dice, playerNumber);
+
+
+    if (dice === 1) {
+        current(0, playerNumber);
+    }
+
+});
+
+
