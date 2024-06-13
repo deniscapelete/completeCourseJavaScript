a4ed0d064b4d382e80976a295567a9fa07792a74
Denis <capelete.denis@hotmail.com>
2024-06-13 20:57:37 -0300

    Part 8 - Primitives vs. Objects in Practice (criando copias)

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -172,81 +172,156 @@ f(); // Retorna indefinido porque agora é uma chamda de uma função regular, n
 
 
 
-const jonas = {
-    firstName: 'Jonas',
-    year: 1991,
-    calcAge: function () {
-        console.log(this); // 'this' refere-se ao objeto jonas
-        console.log(2037 - this.year); // funciona corretamente, pois this.year é 1991
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this); // 'this' refere-se ao objeto jonas
+//         console.log(2037 - this.year); // funciona corretamente, pois this.year é 1991
 
 
-        ///////////////////// 
+//         ///////////////////// 
 
-        // const isMillenial = function () {
-        //     console.log(this);
-        //     console.log(this.year >= 1981 && this.year <= 1996);
-        // };
-        // isMillenial(); // dentro de uma chamada de função regular a palavra chave é indefinida, (função dentro de função)
-        //a função regular obtém sua prorpia palavra chave por isso da erro
-        //////////////////////////  Abaixo segue duas maneiras para corrigir esse problema
-
-
-
-        ///////////////////// Solução 1 - antiga pré-ES6
-
-        // const self = this; // Criamos uma variavel para ter acesso as palavras-chave para jonas
-        // const isMillenial = function () {
-        //     console.log(self);
-        //     console.log(self.year >= 1981 && self.year <= 1996);
-        // };
-        // isMillenial(); 
-
-        /////////////////////
+//         // const isMillenial = function () {
+//         //     console.log(this);
+//         //     console.log(this.year >= 1981 && this.year <= 1996);
+//         // };
+//         // isMillenial(); // dentro de uma chamada de função regular a palavra chave é indefinida, (função dentro de função)
+//         //a função regular obtém sua prorpia palavra chave por isso da erro
+//         //////////////////////////  Abaixo segue duas maneiras para corrigir esse problema
 
 
 
+//         ///////////////////// Solução 1 - antiga pré-ES6
 
-        ///////////////////// Solução 2 - atual no ES6    
-        //funciona com a função de seta pois ela não tem sua propria palavra chave, ela usa do escopo pai, neste caso é de calcAge
+//         // const self = this; // Criamos uma variavel para ter acesso as palavras-chave para jonas
+//         // const isMillenial = function () {
+//         //     console.log(self);
+//         //     console.log(self.year >= 1981 && self.year <= 1996);
+//         // };
+//         // isMillenial(); 
 
-        const isMillenial = () => {
-            console.log(this);
-            console.log(this.year >= 1981 && this.year <= 1996);
-        };
-        isMillenial();
+//         /////////////////////
 
 
 
 
-    },
+//         ///////////////////// Solução 2 - atual no ES6    
+//         //funciona com a função de seta pois ela não tem sua propria palavra chave, ela usa do escopo pai, neste caso é de calcAge
 
-    greet: () => console.log(`Hey ${this.firstName}`), // 'this' refere-se ao escopo global, nunca utilizar para definir um método (igual aqui)
+//         const isMillenial = () => {
+//             console.log(this);
+//             console.log(this.year >= 1981 && this.year <= 1996);
+//         };
+//         isMillenial();
+
+
+
+
+//     },
+
+//     greet: () => console.log(`Hey ${this.firstName}`), // 'this' refere-se ao escopo global, nunca utilizar para definir um método (igual aqui)
+// };
+
+// jonas.greet();// 'this' refere-se ao escopo global, onde 'firstName' é undefined3 
+// jonas.calcAge(); // 'this' refere-se a 'jonas', então o cálculo funciona corretamente 
+
+
+
+
+// //argument keyword
+// const addExpr = function (a, b) {
+//     console.log(arguments);
+//     return a + b;
+
+// };
+
+
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12);
+// //Com a palavra chave arguments, mesmo passando apenas dois parametros consigo incluir mais argumentos além desses dois, funciona apenas em função regular.
+
+
+// var addArrow = (a, b) => {
+//     console.log(arguments);
+//     return a + b;
+// }
+
+// console.log(addArrow(2, 5, 8));
+
+// //A palavra chave arguments não funciona em função de seta, dessa forma os argumentos não são definidos 
+
+
+
+
+
+
+// ************** Primitive types **************
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis'
+console.log(lastName, oldLastName);
+
+
+// ************** Reference types **************
+const jessica = {
+    firstName: 'Jessica',
+    lastame: 'Willians',
+    age: 27,
 };
 
-jonas.greet();// 'this' refere-se ao escopo global, onde 'firstName' é undefined
-jonas.calcAge(); // 'this' refere-se a 'jonas', então o cálculo funciona corretamente 
+
+//Aqui ele não copia ele simplesmente utiliza a mesma referencia salva no heap.
+//Logo se eu fizer qualquer alteração  em 'marriedJessica' ele ira alterar tambem em 'jessica'
+const marriedJessica = jessica;
+
+marriedJessica.lastName = 'Davis';
+console.log(jessica);
+console.log(marriedJessica)
 
 
+//não é possivel fazer isso pois estaria criando um novo objeto, 
+//pois esse novo objto será armazenado em um lugar diferente da memória,a referencia na variavel tera que mudar por ser uma constante
+//marriedJessica = {}; 
+
+//Alterar completamente um objeto é diferente de alterar apenas as propriedade
 
 
-//argument keyword
-const addExpr = function (a, b) {
-    console.log(arguments);
-    return a + b;
+// ************** Copying objects **************
 
+
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Jose',
+    age: 55,
 };
 
 
-addExpr(2, 5);
-addExpr(2, 5, 8, 12);
-//Com a palavra chave arguments, mesmo passando apenas dois parametros consigo incluir mais argumentos além desses dois, funciona apenas em função regular.
+
+//cria apenas uma copia superficial
+//se houver um objeto dentro de outro, o objeto interno apontara para o mesmo lugar na memória
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Arroba';
+
+console.log(jessica2);
+console.log(jessicaCopy);
 
 
-var addArrow = (a, b) => {
-    console.log(arguments);
-    return a + b;
-}
 
-console.log(addArrow(2, 5, 8));
+//************** Exemplo da copia superficial **************
 
-//A palavra chaver arguments não funciona em função de seta, dessa forma os argumentos não são definidos 
+const jessica3 = {
+    firstName: 'Jessica',
+    lastName: 'Jose',
+    age: 55,
+    family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy3 = Object.assign({}, jessica3);
+jessicaCopy.lastName = 'Arroba';
+
+jessicaCopy3.family.push('Mary');
+//com essa alteração family passa a ter 4 em 'jessicaCopy3' e em 'jessica', pois ambos utilizam a mesma referencia no heap
+
+console.log(jessica3);
+console.log(jessicaCopy3);
