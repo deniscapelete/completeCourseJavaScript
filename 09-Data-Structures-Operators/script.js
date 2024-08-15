da71d09dc752f561b50490771d0519e1e03b5303
Denis <capelete.denis@hotmail.com>
2024-08-15 20:43:03 -0300

    126. String Methods Practice

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -58,7 +58,34 @@ const restaurant = {
 
 
 ///////////////////////////////////////
-// Coding Challenge #4
+// String Methods Practice
+
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'))
+
+const getCode = str => str.slice(0, 3).toUpperCase()
+
+for (const flight of flights.split('+')) {
+
+  const [type, from, to, time] = flight.split(';');
+
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(45);
+
+  console.log(output)
+}
+
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+
+
+
+
+// // // // // // -------------- 125. Coding Challenge #4 --------------
 
 /*
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
@@ -121,33 +148,33 @@ Após isso, teste com seus próprios dados!
 BOA SORTE 😀
 */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
 
 
-document.querySelector('button').addEventListener('click', function () {
+// document.querySelector('button').addEventListener('click', function () {
 
-  const text = document.querySelector('textarea').value;
+//   const text = document.querySelector('textarea').value;
 
-  // const textLowerCase = text.toLowerCase();
+//   // const textLowerCase = text.toLowerCase();
 
-  const rows = text.split('\n');
+//   const rows = text.split('\n');
 
-  // console.log(rows)
+//   // console.log(rows)
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
 
 
-    const output = `
-    ${first}${second.replace(second[0], second[0].toUpperCase())}`;
+//     const output = `
+//     ${first}${second.replace(second[0], second[0].toUpperCase())}`;
 
-    console.log(`${output.padEnd(22)}${'✅'.repeat(i + 1)}`);
+//     console.log(`${output.padEnd(22)}${'✅'.repeat(i + 1)}`);
 
-  }
+//   }
 
-})
+// })
 
 
 // console.log(text)
