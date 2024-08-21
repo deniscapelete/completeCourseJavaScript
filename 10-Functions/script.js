bd6e9b6a85b6ee8798ba05fe774de2a5bbca1d84
Denis <capelete.denis@hotmail.com>
2024-08-21 10:10:05 -0300

    Como funciona a passagem de argumentos: valor versus referência

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -4,6 +4,7 @@
 //------------- 129. Default Parameters -------------
 //valor padrão
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -35,3 +36,63 @@ createBooking('ABX425', undefined, 4)//Uma maneira para resolver isso é adicion
 //funciona pois, fazendo isso é a mesma coisa que nem configura-lo, já que quando não passamos o argumento ele fica como UNDEFINED
 
 console.log(bookings)
+*/
+
+
+
+
+//------------- 130. How Passing Arguments Works: Value vs. Reference -------------
+//Como funciona a passagem de argumentos: valor versus referência
+
+const flight = 'LH234'; // PRIMITIVO
+const denis = {
+    name: 'Denis Capelete',
+    passport: 45685265423
+}
+
+const checkIn = function (flightNum, passenger) { // 
+    flightNum = 'LH999'; //o PARAMETRO flightNum funciona basicamente como uma cópia do valor original, portanto essa mudança não altera de fato o "flight"
+    passenger.name = 'Mr. ' + passenger.name // Quando passamos um tipo de referência para uma função, é copiado a referência ao objeto no heap de memória
+
+    // é apenas um exemplo, não devemos alterar o valor de um PARÂMETRO
+
+
+    if (passenger.passport === 45685265423) {
+        alert('Checked in');
+    } else {
+        alert('Wrong passport!');
+    }
+
+}
+//passar valor PRIMITIVO para uma função é p mesmo que criar uma cópia
+
+//passar OBJETO cria uma copia simples utilizando o mesmo espaço no HEAP
+
+checkIn(flight, denis)
+console.log(flight);
+console.log(denis)
+
+//é o mesmo que fazer...
+const flightNum = flight
+// faz uma copia, se o flightNum for alterado o fligth não sofre alteração.
+const passenger = denis  // passenger e denis apontam para o mesmo objeto na memória.
+// copia a mesma referência no HEAP, então se alterar o 'passenger' o 'denis' também sofre alteração por usarem o mesmo espaço no HEAP de memória.
+
+// Exemplo de problema
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 100000001);
+};
+
+newPassport(denis);// aqui é feita a alteração do número do passaporte no OBJETO
+checkIn(flight, denis); // aqui o objeto foi alterado pela função "newPassport" então o número do passaporte não é mais o mesmo e da falso nossa condição
+
+
+
+
+
+
+
+
+
+
+
