8f096509f71badc1e87d310ab0c949440dd1db09
Denis <capelete.denis@hotmail.com>
2024-08-31 18:28:22 -0300

    136. Coding Challenge #1, question 1.1

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -162,124 +162,226 @@ console.log(bookings)
 
 // // //------------- 134. The call and apply Methods  -------------
 
-const lufthansa = {
-    airline: 'Lufthansa',
-    iataCode: 'LH',
-    bookings: [],
-    book(flightNum, name) {
-        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
-        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
-    },
-}
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     book(flightNum, name) {
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+//         this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
+//     },
+// }
 
-lufthansa.book(210, 'Denis Teste');
-lufthansa.book(542, 'Jonas Testando');
-console.log(lufthansa)
+// lufthansa.book(210, 'Denis Teste');
+// lufthansa.book(542, 'Jonas Testando');
+// console.log(lufthansa)
 
-const eurowings = {
-    airline: 'Eurowings',
-    iataCode: 'EW',
-    bookings: [],
-};
-// Precisamos usar exatamente os mesmos nome das 'PROPRIEDADE' do objeto original, pois O 'MÉTODO BOOK' está tentando ler apenas essas propriedades
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: [],
+// };
+// // Precisamos usar exatamente os mesmos nome das 'PROPRIEDADE' do objeto original, pois O 'MÉTODO BOOK' está tentando ler apenas essas propriedades
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
-// Não funciona (Does NOT work)
-// book(23, 'Joao Pedro'); 
-// a palavra-chave 'THIS' a qual 'BOOK' se refere nesse caso está apontando para 'UNDEFINED'
-
-
-
-// ------- Método de chamada - (Call method) -------
-// Definindo manual e explicitamente a palavra-chave(this)
-book.call(eurowings, 23, 'Joao Pedro');
-// o método 'CALL', chama a função book com a palavra-chave(this) definida como eurowings
-console.log(eurowings);
-
-book.call(lufthansa, 202, 'Marina Silva');
-console.log(lufthansa);
-
-const swiss = {
-    airline: 'Swiss Air Lines',
-    iataCode: 'LX',
-    bookings: []
-}
-
-book.call(swiss, 282, 'Kaka');
-console.log(swiss);
+// // Não funciona (Does NOT work)
+// // book(23, 'Joao Pedro'); 
+// // a palavra-chave 'THIS' a qual 'BOOK' se refere nesse caso está apontando para 'UNDEFINED'
 
 
 
-// ------- Método de aplicação - (Apply method)  -------
-//Não recebe uma lista de argumentos apos á palavra-chave(this), na verdade ele receberá um 'ARRAY' de argumentos.
-const flightData = [320, 'George Lincoln'];
-book.apply(swiss, flightData);
-//NÃO É MAIS UTILIZADO
-console.log(swiss);
+// // ------- Método de chamada - (Call method) -------
+// // Definindo manual e explicitamente a palavra-chave(this)
+// book.call(eurowings, 23, 'Joao Pedro');
+// // o método 'CALL', chama a função book com a palavra-chave(this) definida como eurowings
+// console.log(eurowings);
 
-book.call(swiss, ...flightData)
-//MANEIRA INDICADA
+// book.call(lufthansa, 202, 'Marina Silva');
+// console.log(lufthansa);
+
+// const swiss = {
+//     airline: 'Swiss Air Lines',
+//     iataCode: 'LX',
+//     bookings: []
+// }
+
+// book.call(swiss, 282, 'Kaka');
+// console.log(swiss);
+
+
+
+// // ------- Método de aplicação - (Apply method)  -------
+// //Não recebe uma lista de argumentos apos á palavra-chave(this), na verdade ele receberá um 'ARRAY' de argumentos.
+// const flightData = [320, 'George Lincoln'];
+// book.apply(swiss, flightData);
+// //NÃO É MAIS UTILIZADO
+// console.log(swiss);
+
+// book.call(swiss, ...flightData)
+// //MANEIRA INDICADA
 
 
 
 // // //------------- 135. The bind Method  -------------
 
-// ------- Método de bind - (Bind method)  -------
-// Assim como 'CALL' permite definir manualmente as palavras-chave(this)
-// O 'BIND' não chama a função imediatamente, ele retorna uma nova função onde a palavra-chave(this) está associada.
-// É necessario chamar a função posteriormente quando for usar
+// // ------- Método de bind - (Bind method)  -------
+// // Assim como 'CALL' permite definir manualmente as palavras-chave(this)
+// // O 'BIND' não chama a função imediatamente, ele retorna uma nova função onde a palavra-chave(this) está associada.
+// // É necessario chamar a função posteriormente quando for usar
 
-const bookLH = book.bind(lufthansa);
-const bookEW = book.bind(eurowings);
-const bookLX = book.bind(swiss);
+// const bookLH = book.bind(lufthansa);
+// const bookEW = book.bind(eurowings);
+// const bookLX = book.bind(swiss);
 
-bookEW(425, 'Jorginho');
-console.log(eurowings);
+// bookEW(425, 'Jorginho');
+// console.log(eurowings);
 
-const bookEW23 = book.bind(eurowings, 23);
-//Neste caso deixamos predefinido o primeiro parâmetro (padrão chamado de aplicação parcial)
-bookEW23('Denis teste'); // retorna -  Denis teste booked a seat on Eurowings flight EW23
-bookEW23('Marta testando') // retorna - Marta testando booked a seat on Eurowings flight EW23
+// const bookEW23 = book.bind(eurowings, 23);
+// //Neste caso deixamos predefinido o primeiro parâmetro (padrão chamado de aplicação parcial)
+// bookEW23('Denis teste'); // retorna -  Denis teste booked a seat on Eurowings flight EW23
+// bookEW23('Marta testando') // retorna - Marta testando booked a seat on Eurowings flight EW23
 
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-    console.log(this);
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//     console.log(this);
 
-    this.planes++
-    console.log(this.planes);
-};
-// lufthansa.buyPlane()
-
-
-document.querySelector('.buy')
-    .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+//     this.planes++
+//     console.log(this.planes);
+// };
+// // lufthansa.buyPlane()
 
 
-// Partial application
-
-const addTax = (rate, value) => value + value * rate;
-
-console.log(addTax(0.1, 200));
+// document.querySelector('.buy')
+//     .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 
-const addVAT = addTax.bind(null, 0.23); // neste caso não importa a palavra-chave(this) então adicionamos null.
+// // Partial application
 
-console.log(addVAT(100));
-console.log(addVAT(200));
+// const addTax = (rate, value) => value + value * rate;
 
-
-// // const addNewTax = rate => value =>  value + value * rate; (com arrow function)
+// console.log(addTax(0.1, 200));
 
 
-const addNewTax = function (rate) {
-    return function (value) {
-        return value + value * rate;;
+// const addVAT = addTax.bind(null, 0.23); // neste caso não importa a palavra-chave(this) então adicionamos null.
+
+// console.log(addVAT(100));
+// console.log(addVAT(200));
+
+
+// // // const addNewTax = rate => value =>  value + value * rate; (com arrow function)
+
+
+// const addNewTax = function (rate) {
+//     return function (value) {
+//         return value + value * rate;;
+//     }
+// }
+
+// const newTax = addNewTax(0.23);
+
+// console.log(newTax(200));
+// console.log(newTax(100));
+
+
+// // //------------- 136. Coding Challenge #1  -------------
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+
+/////////////////////////////////////// // Desafio de Codificação #1
+
+/* Vamos construir um simples aplicativo de enquete!
+
+Uma enquete tem uma pergunta, um array de opções para as quais as pessoas podem escolher e um array com o número de respostas para cada opção.
+Esses dados estão armazenados no objeto inicial abaixo.
+
+Aqui estão suas tarefas:
+
+Crie um método chamado 'registerNewAnswer' no objeto 'poll'. O método faz 2 coisas:
+
+1.1. Exibe uma janela de prompt para o usuário inserir o número da opção selecionada. 
+    O prompt deve ser assim: Qual é a sua linguagem de programação favorita? 0: JavaScript 1: Python 2: Rust 3: C++ C
+
+1.2. Com base no número de entrada, atualize o array de respostas. Por exemplo, se a opção for 3, aumente o valor NA POSIÇÃO 3 do array em 1.
+     Certifique-se de verificar se a entrada é um número e se o número faz sentido (por exemplo, a resposta 52 não faria sentido, certo?) 2.
+     Chame este método sempre que o usuário clicar no botão "Responder enquete". 3. Crie um método 'displayResults' que exiba os resultados da enquete.
+     O método recebe uma string como entrada (chamada 'type'), que pode ser 'string' ou 'array'.
+     Se o tipo for 'array', simplesmente exiba o array de resultados como está, usando console.log(). Esta deve ser a opção padrão.
+     Se o tipo for 'string', exiba uma string como "Os resultados da enquete são 13, 2, 4, 1". 4.
+     Execute o método 'displayResults' no final de cada chamada do método 'registerNewAnswer'.
+
+DICA: Use muitas das ferramentas que você aprendeu nesta e na última seção 😉
+
+BÔNUS: Use o método 'displayResults' para exibir os 2 arrays nos dados de teste. Use tanto a opção 'array' quanto 'string'. NÃO coloque os arrays no objeto da enquete! Então, como deve ser a palavra-chave this nesse caso?
+
+DADOS DE TESTE BÔNUS 1: [5, 2, 3] DADOS DE TESTE BÔNUS 2: [1, 5, 3, 9, 6, 1]
+
+BOA SORTE 😀 */
+
+
+const poll = {
+    question: 'What is your favourite programming language?',
+    options: [
+        '0: JavaScript',
+        '1: Python',
+        '2: Rust',
+        '3: C++'
+    ],
+    // This generates [0, 0, 0, 0]. More in the next section 😃
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        document.querySelector('.poll').addEventListener('click', function () {
+            console.log(prompt(poll.question + poll.options + '(Escreva o número da opção)'))
+        });
     }
-}
+};
 
-const newTax = addNewTax(0.23);
+const registerNewAnswer = poll.registerNewAnswer;
 
-console.log(newTax(200))
-console.log(newTax(100))
+registerNewAnswer();
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('.poll').addEventListener('click', function () {
+//     console.log(prompt(poll.question + poll.options));
+// })
