a55edfac598d4f3f2e6b9ecd84377081bbb9d29c
Denis <capelete.denis@hotmail.com>
2024-09-24 21:12:00 -0300

    148. Creating DOM Elements

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -61,6 +61,43 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+// // ------------------- 148. Creating DOM Elements -------------------
+const displayMovements = function (movements) {
+
+  containerMovements.innerHTML = '';
+  // removendo tudo do "containerMovements" incluindo as tags html
+
+
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+           <div class="movements__row">
+            <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>           
+            <div class="movements__value">${mov}</div>
+          </div>
+        `;
+    containerMovements.insertAdjacentHTML('afterbegin', html)
+    //adiciona o valor da variável 'html' dentro do "containerMovements" arquivo HTML
+  });
+
+};
+displayMovements(account1.movements);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
