bd6cae805ce227285cd57d0823b1ef9e6baab22e
Denis <capelete.denis@hotmail.com>
2024-09-30 08:49:24 -0300

    157. CHALLENGE #3 minha resolução

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -607,7 +607,7 @@ const calcAverageHumanAge = function (ages) {
   const med = ages
     .map(age => (age <= 2) ? 2 * age : 16 + age * 4)
     .filter(age => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0)
   return med;
 }
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
