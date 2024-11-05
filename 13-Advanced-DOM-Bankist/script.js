e47414683513cf431c0c81bae63936395e72939c
Denis <capelete.denis@hotmail.com>
2024-11-04 21:36:46 -0300

    193. Event Delegation: Implementing Page Navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -334,3 +334,37 @@ Adicionando true ao evento do .nav
   Sem true, o código entraria diretamente na fase de borbulhamento, e o evento nos elementos internos seria tratado primeiro.
 
 */
+
+
+// ------------ 193. Event Delegation: Implementing Page Navigation ------------
+
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+
+
+// 1. Add Event Listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+});
