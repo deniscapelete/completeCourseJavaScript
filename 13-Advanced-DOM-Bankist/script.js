9b28b063731ee6f1cb24731c2041fb3888f35224
Denis <capelete.denis@hotmail.com>
2024-11-22 18:33:26 -0300

    198. A Better Way: The Intersection Observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -447,10 +447,26 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // ----------------- 197. Implementing a Sticky Navigation: The Scroll Event -----------------
 
 // Stick navigation
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-console.log(initialCoords);
-window.addEventListener('scroll', function (e) {
-  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
-});
+// console.log(initialCoords);
+// window.addEventListener('scroll', function (e) {
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// ----------------- 198. A Better Way: The Intersection Observer API -----------------
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
