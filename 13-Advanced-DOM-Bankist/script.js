29d6d50f6eb5da25da0fa07d645740d1435af9aa
Denis <capelete.denis@hotmail.com>
2024-11-28 20:14:12 -0300

    201. Building a Slider Component: Part 1

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -529,13 +529,37 @@ const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
 
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.4) translateX(-800px)';
-slider.style.overflow='visible';
+let curSlide = 0;
 
-slides.forEach((s,i)=> (s.style.transform = `translatex(${100*i}%)`));
+const maxSlide = slides.length;
+
+
+// slides.forEach((s,i)=> (s.style.transform = `translatex(${100*i}%)`));
+
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i -slide)}%)`))
+};
+goToSlide(0)
+
 
 // Next slide
-btnRight.addEventListener('click', function(){
+const nextSlide = function(){
+  if(curSlide === maxSlide - 1){
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+}
 
-});
+const prevSlide = function(){
+  if(curSlide === 0){
+    curSlide = (maxSlide - 1);
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+}
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
