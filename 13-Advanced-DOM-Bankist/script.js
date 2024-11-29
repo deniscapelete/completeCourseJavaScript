cf25faa5bb1ecd7bfdd3084d7ac52cf22b0d02b4
Denis <capelete.denis@hotmail.com>
2024-11-28 21:31:47 -0300

    202. Building a Slider Component: Part 2

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -525,21 +525,42 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 
 // Slider
+const slider = function (){
+
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 
 const maxSlide = slides.length;
 
 
-// slides.forEach((s,i)=> (s.style.transform = `translatex(${100*i}%)`));
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow='visible';
+
+
+// Functions
+const createDots = function (){
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+  })
+};
+
+
+const activateDot = function (slide){
+ document.querySelectorAll('.dots__dot')
+.forEach(dot => dot.classList.remove('dots__dot--active'));
+document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+
 
 const goToSlide = function(slide) {
   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i -slide)}%)`))
 };
-goToSlide(0)
+
 
 
 // Next slide
@@ -550,6 +571,7 @@ const nextSlide = function(){
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 }
 
 const prevSlide = function(){
@@ -559,7 +581,31 @@ const prevSlide = function(){
     curSlide--;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 }
+const init = function(){
+  goToSlide(0)
+  createDots();
+  activateDot(0);  
+};
+init();
 
+//Event handlers
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e){ 
+  if(e.key === 'ArrowLeft') prevSlide();
+  e.key === 'ArrowRight' && nextSlide();
+})
+
+dotContainer.addEventListener('click', function(e){
+  if (e.target.classList.contains('dots__dot')){
+       const {slide} = e.target.dataset;  // desestruturação -> e a mesma coisa que const slide = e.target.dataset.slide;  
+       curSlide = Number(slide);
+       goToSlide(slide);
+    activateDot(slide);
+  }
+})
+};
+slider();
