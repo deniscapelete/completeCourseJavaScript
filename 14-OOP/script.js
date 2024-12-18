f9a164357692510b312773c235c64c4822fb1d8b
Denis <capelete.denis@hotmail.com>
2024-12-18 20:54:30 -0300

    231. Inheritance Between 'Classes': ES6 Classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -471,3 +471,76 @@ tesla.accelerate();
 tesla.accelerate();
 tesla.accelerate();
 tesla.brake();
+
+
+
+/* --------------------------- 231. Inheritance Between "Classes": ES6 Classes --------------------------- */
+
+// class declaration
+  class PersonCl {
+    constructor(fullName, birthYear){
+      this.fullName = fullName;
+      this.birthYear = birthYear;
+    }
+
+    //Methods will be added to .prototype property
+    //todos os metodos escritos fora do construtor estará no protótipo(prototype), e não no próprio objeto
+    calcAge(){
+      console.log(2024 - this.birthYear);
+    }
+
+   greet(){
+      console.log(`Olá ${this.fullName}`);
+    }
+
+      get age(){
+        return 2024 - this.birthYear;    
+      }
+
+  // Set a property that allready exists
+      set fullName(name){
+        console.log(name);
+        if(name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+      }
+
+      get fullName(){
+        return this._fullName;
+      }
+  }
+
+  const jessica = new PersonCl('Jessica Davis', 2000);
+  
+  console.log(jessica);
+  jessica.calcAge();
+  console.log(jessica.age);
+
+  console.log(jessica.__proto__ == PersonCl.prototype);
+
+  // PersonCl.prototype.greet = function (){
+  //   console.log(`Olá ${this.firstName}`);
+  // }
+
+  jessica.greet();
+
+  class studentCl extends PersonCl {
+    constructor(fullName, birthYear, course){
+      // Always needs to happen fisrt! o super deve vir primeiro, pois precisa gravar a palavra chave no disco
+      super(fullName, birthYear);
+      this.course = course;
+    }
+    introduce(){
+      console.log(`Meu nome é ${this.fullName} e eu estudo ${this.course}`)
+    }
+
+    calcAge(){
+      console.log((`Olá tenho ${2024-this.birthYear}`));
+    }
+  }
+
+  const marta = new studentCl('marta jones', 2002, 'Computer Science');
+  console.log(marta);
+
+  marta.introduce();
+  marta.calcAge();
+
