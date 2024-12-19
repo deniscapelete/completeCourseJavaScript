4be79986c42aba089db31d23c06336b171d93df8
Denis <capelete.denis@hotmail.com>
2024-12-19 10:34:54 -0300

    233. Another Class Example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -580,3 +580,53 @@ joao.init('Joao', 1999, 'Adm');
 
 joao.introduce();
 joao.calcAge();
+
+
+  /* --------------------------- 233. Another Class Example --------------------------- */
+
+  class Account {
+    constructor (owner, currency, pin) {
+      this.owner = owner;
+      this.currency = currency;
+      this.pin = pin;
+      this.movements = [];
+      this.locale = navigator.language;
+
+      console.log(`Thanks for opening an account, ${owner}`);      
+    }
+// Public interface
+    deposit(val) {
+      this.movements.push(val);
+    }
+
+    withdraw(val) {
+      this.deposit(-val);
+    }
+
+    approveLoan(val){
+      return true;
+    }
+
+    requestLoan(val){
+      if(this.approveLoan(val)){
+        this.deposit(val);
+        console.log(`Loan approved`);
+      }
+    }
+
+  }
+
+  const acc1 = new Account ('Denis', 'EUR', 1111);
+  console.log(acc1);
+
+  /*
+      NÃO É UMA BOA IDEIA FAZER DESSA FORMA, é melhor criar metodos que interagem com essas propriedades
+    acc1.movements.push(320);
+    acc1.movements.push(-110); 
+  */
+  acc1.deposit(135);
+  acc1.withdraw(135);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000); // esse método não deveria se acessivel aqui (é necessário deixar privado)
+
+  console.log(acc1);
